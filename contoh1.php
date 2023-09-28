08f31e2121314fc30da1b330f9196280cdc83415
Itzsatt <handrianussatriowibowoh@gmail.com>
2023-09-28 16:09:43 +0700

    berikut tugas contoh c1 controller

---
+++ contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya adalah Handrianus Satrio W, Saya tinggal di solo dan hobi saya mendengerkan musik";
+    }
+}
