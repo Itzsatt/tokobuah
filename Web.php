e8d95faa0663e00685be2c9c203c12f2e9e0e1df
Itzsatt <handrianussatriowibowoh@gmail.com>
2023-10-19 18:24:33 +0700

    Menambahkan web, v_index, v_header, v_footer, v_about, stylebuku dan screenshoot

--- Web.php
+++ Web.php
@@ -1,9 +1,11 @@
 <?php 
 defined('BASEPATH') or exit ('no direct script access allowed'); 
+
 class Web extends CI_Controller{ 
     
     function __construct(){ 
-        parent::__construct(); 
+        parent::__construct();
+        $this->load->helper('url');
     } 
     
     public function index(){ 
@@ -13,8 +15,7 @@ class Web extends CI_Controller{
         $this->load->view('v_footer',$data); 
     } 
 
-    public function about()
-    {
+    public function about(){
         $data['judul'] = "Halaman About"; 
         $this->load->view('v_header',$data); 
         $this->load->view('v_about',$data);
--- v_about.php
+++ v_about.php
@@ -2,22 +2,22 @@
     <h1><?php echo $judul ?></h1>
     <h4>Nama</h4> 
     <ul type="disc"> 
-        <li>Nama Depan : Imam</li> 
-        <li>Nama Belakang : Nawawi</li> 
+        <li>Nama Depan : Handrianus</li> 
+        <li>Nama Belakang : Wibowo</li> 
     </ul> 
     <br> 
     <h4>Alamat</h4> 
     <ul type="none"> 
-        <li> Jalan Ciledug Raya No. 168 Pesanggrahan</li> 
+        <li> Jalan teuku cik ditiro rt 03 rw 09 karanglo madegondo solobaru</li> 
     </ul> 
     <h4>Tempat Lahir</h4> 
     <ul type="none"> 
-        <li>Cirebon</li> 
+        <li>Karanganyar</li> 
     </ul> 
     
     <h4>Olah Raga Favorit</h4> 
     <ul type="square"> 
-        <li>Bulutangkis</li> 
-        <li>Catur</li> 
+        <li>Sepedaan</li> 
+        <li>Lari</li> 
     </ul> 
 </section>
